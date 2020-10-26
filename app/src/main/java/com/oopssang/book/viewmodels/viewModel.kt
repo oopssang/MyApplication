9196d568ab6861ebb4a6e93ce7f180ec354df1bc
Lee-SangHyeok <oopssang@gmail.com>
2020-10-27 00:03:35 +0900

    kakao api 붙임2

--- app/src/main/java/com/oopssang/book/viewmodels/viewModel.kt
+++ app/src/main/java/com/oopssang/book/viewmodels/viewModel.kt
@@ -1,9 +1,29 @@
-//compackage com.oopssang.book.viewmodels
-//
-//private val viewModel: MainViewModel by lazy {
-//    ViewModelProvider(this@MainActivity, object : ViewModelProvider.Factory {
-//        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-//            return MainViewModel() as T
-//        }
-//    }).get(MainViewModel::class.java)
-//}
+package com.oopssang.book.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.oopssang.book.api.service.SearchBookService
+import kotlinx.coroutines.launch
+
+
+class SearchViewModel(
+    private val loginRepository: LoginRepository
+): ViewModel() {
+
+    fun search(bookName: String) {
+
+        // Create a new coroutine on the UI thread
+        viewModelScope.launch {
+
+            // Make the network call and suspend execution until it finishes
+            val response = SearchBookService.create().searchBook(bookName, 1, 50)
+
+            // Display result of the network request to the user
+            when (response) {
+
+
+            }
+        }
+    }
+}
+
